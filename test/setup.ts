394d03866628d01b3f225884717533f6df7743a4
Robin Heinze <robin.m.heinze@gmail.com>
2018-05-25 10:29:23 -0700

    Fix tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,10 @@
+// Storybook will be using `module` as the 2nd parameter
+// to storiesOf, but in our TypeScript setup, we don't have
+// module defined under global.
+declare global {
+  var module
+}
+
 // we always make sure 'react-native' gets included first
 import "react-native"
 
