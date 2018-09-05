000026d1a880ea4e480602dc07c28fb2597ff356
Morgan Laco <morganlaco@gmail.com>
2018-09-05 08:08:31 -0400

    Add background image

--- src/views/example/drawer/drawer-screen-1.tsx
+++ src/views/example/drawer/drawer-screen-1.tsx
@@ -1,11 +1,23 @@
 import * as React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native"
 
 export default class DrawerScreen1 extends React.Component {
   render() {
+    // const im = require("./illustration_screen1.png")
+    const im = require("./bg-gradient.png")
     return (
       <View style={styles.container}>
-        <Text>Screen 1</Text>
+        <ImageBackground
+          source={im}
+          style={styles.backgroundImage}
+          // imageStyle={styles.image}
+          // resizeMode="cover"
+        >
+          {/* <Image
+            source={im}
+            style={styles.image}
+          /> */}
+        </ImageBackground>
       </View>
     )
   }
@@ -13,9 +25,18 @@ export default class DrawerScreen1 extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    // flex: 1,
+    // backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    // paddingHorizontal: "50%",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+  },
+  image: {
+    height: "20%",
+    width: "56%",
   },
 })
